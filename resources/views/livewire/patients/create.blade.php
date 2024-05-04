d0ef873ff839a21aff62cc61b04b53d239f2494b
Guilherme Policarpo <guilhermepolicarpo@outlook.com>
2024-05-04 01:20:52 -0300

    layout improvements

--- resources/views/livewire/patients/create.blade.php
+++ resources/views/livewire/patients/create.blade.php
@@ -49,8 +49,8 @@
                     <x-header title="Endereço" subtitle="Endereço do assistido" size="text-1xl" />
                 </div>
                 <div class="grid col-span-4 gap-3">
-                    <div class="grid gap-5 md:grid-cols-3">
-                        <div class="col-span-1">
+                    <div class="grid grid-cols-2 gap-5 md:grid-cols-3">
+                        <div class="col-span-1 md:col-span-1">
 
                             <x-input label="CEP" wire:model="address.zip_code" x-on:blur="$wire.getAddressByZipCode($event.target.value)" icon="o-map-pin" placeholder="Digite o CEP" x-mask="99999-999"  />
                         </div>
@@ -60,23 +60,23 @@
 
                         </div>
                     </div>
-                    <div class="grid gap-5 md:grid-cols-5">
-                        <div class="col-span-1">
+                    <div class="grid grid-cols-5 gap-5">
+                        <div class="col-span-2 md:col-span-1">
 
                             <x-input label="Número" wire:model="address.number" placeholder="123" />
 
                         </div>
-                        <div class="col-span-2">
+                        <div class="col-span-3 md:col-span-2">
 
                             <x-input label="Bairro" phone wire:model="address.neighborhood" placeholder="Centro" />
 
                         </div>
-                        <div class="col-span-2">
+                        <div class="col-span-5 md:col-span-2">
 
                             <x-input label="Cidade" phone wire:model="address.city" placeholder="Campo Florido" />
 
                         </div>
-                        <div class="col-span-2">
+                        <div class="col-span-5 md:col-span-2">
 
                             <x-select label="Estado" :options="$states" option-value="acronym" option-label="name" placeholder-value="MG"
                                 placeholder="Selecione um estado" wire:model="address.state" />
