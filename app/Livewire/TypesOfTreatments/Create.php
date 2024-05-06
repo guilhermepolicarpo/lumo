f7e7cac5a7e3eed2c955b977e329e4836ed00e30
Guilherme Policarpo <guilhermepolicarpo@outlook.com>
2024-05-06 12:36:46 -0300

    created type of treatment create

--- app/Livewire/TypesOfTreatments/Create.php
+++ app/Livewire/TypesOfTreatments/Create.php
@@ -2,12 +2,53 @@
 
 namespace App\Livewire\TypesOfTreatments;
 
+use App\Models\TypeOfTreatment;
+use Mary\Traits\Toast;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class Create extends Component
 {
+    use Toast;
+
+    public string $name = '';
+    public string $description = '';
+    public bool $is_the_healing_touch = false;
+    public bool $has_form = false;
+
+
+    #[Title('Adicionar Tipo de Tratamento')]
     public function render()
     {
         return view('livewire.types-of-treatments.create');
+    }
+
+
+    public function save(): void
+    {
+        $validatedData = $this->validate();
+
+        $typeOfTreatment = TypeOfTreatment::create($validatedData);
+
+        $this->success('Orientação adicionada.', description: "A orientação {$typeOfTreatment->name} foi adicionada.", redirectTo: route('types-of-treatments.index'));
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:2|max:255|unique:types_of_treatments,name',
+            'description' => 'nullable|string|min:2',
+            'is_the_healing_touch' => 'required|boolean',
+            'has_form' => 'required|boolean',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Informe o nome do tipo de atendimento.',
+            'name.unique' => 'Já existe um tipo de atendimento com este nome.',
+        ];
     }
 }
--- resources/views/livewire/types-of-treatments/create.blade.php
+++ resources/views/livewire/types-of-treatments/create.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    {{-- HEADER --}}
+    <x-header title="Adicionar Tipo de Atendimento" size="text-2xl" progress-indicator />
+
+    <x-card class="max-w-xl">
+        <x-form wire:submit="save">
+
+            <x-input label="Nome" wire:model="name" icon="o-document-text" placeholder="Passe com todos" />
+
+            <x-textarea label="Descrição" wire:model="description" placeholder="Digite uma descrição..." rows="5" />
+
+            <x-toggle label="Este atendimento é um tipo de passe?" wire:model="is_the_healing_touch" />
+
+            <x-toggle label="Habilitar formulário para este atendimento?" wire:model="has_form" />
+
+            <x-slot:actions>
+                <x-button label="Cancelar" link="{{ route('types-of-treatments.index') }}" class="text-base" />
+                <x-button label="Adicionar" icon="o-paper-airplane" spinner="save" type="submit"
+                    class="text-base btn-primary" />
+            </x-slot:actions>
+
+        </x-form>
+    </x-card>
 </div>
